1862282c1694cae0e54098842c169b596aee3555
a.jamshidi <alirezajam98@gmail.com>
2024-10-26 12:04:02 +0330

    add copy writing test info page on kyc

--- pages/select_server_page.py
+++ pages/select_server_page.py
@@ -2,14 +2,14 @@ from appium.webdriver.common.appiumby import AppiumBy
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
-from pages.kyc.create_account_info_page import CreateAccountInfoPage
+from pages.kyc.kyc_pages import CreateAccountInfoPage
 
 
 class SelectServerBottomSheet(BasePage):
     def click_uat(self):
-        have_account_button = self.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className('
+        click_uat_button = self.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className('
                                                                               '"android.view.ViewGroup").instance(3)')
-        have_account_button.click()
+        click_uat_button.click()
 
         return CreateAccountInfoPage(self.driver)
 
